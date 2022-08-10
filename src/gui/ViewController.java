796598f3058ead4dc201955e4e065cedaebe974c
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-08-09 21:06:50 -0300

    Update small calculator

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,13 +1,18 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ViewController {
+public class ViewController implements Initializable{
 
 	@FXML
 	private TextField txtNumber1;
@@ -31,10 +36,21 @@ public class ViewController {
 
 	@FXML
 	public void onBtSomaAction() {
-		double number1 = Double.parseDouble(txtNumber1.getText());
-		double number2 = Double.parseDouble(txtNumber1.getText());
-		double soma = number1 + number2;
-		labelResult.setText(String.format("%.2f", soma));
+		try {
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			double soma = number1 + number2;
+			labelResult.setText(String.format("%.2f", soma));
+			System.out.println(soma);
+		} catch (NumberFormatException e) {
+			Alerts.showAlert("ERROR", "Erro de parse", "Não é possível calcular números", AlertType.ERROR);
+		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.setTextFieldDouble(txtNumber1);
+		Constraints.setTextFieldDouble(txtNumber2);
 	}
 
 }
